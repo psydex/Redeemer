22051523fdcd8ce3d57faefa34657a291e3122c4
psydex <psydex@duck.com>
2026-02-09 09:43:11 +0200

    Update Redeemer.user.js
    version 2

--- Redeemer.user.js
+++ Redeemer.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name        The Redeemer
+// @name        Redeemer
 // @namespace   psydex
 // @description Pops up the Steam product activation webpage when copying keys from any website. Does not require Steam to be installed!
 // @include     *
-// @version     2.2
+// @version     2.3
 // @grant       none
-// @homepageURL https://github.com/psydex/TheRedeemer/
-// @downloadURL https://raw.githubusercontent.com/psydex/TheRedeemer/master/TheRedeemer.js
-// @updateURL   https://raw.githubusercontent.com/psydex/TheRedeemer/master/TheRedeemer.js
+// @homepageURL https://github.com/psydex/Redeemer/
+// @downloadURL https://github.com/psydex/TheRedeemer/raw/refs/heads/master/Redeemer.user.js
+// @updateURL   https://github.com/psydex/TheRedeemer/raw/refs/heads/master/Redeemer.user.js
 // ==/UserScript==
 (function() {
 	"use strict";
